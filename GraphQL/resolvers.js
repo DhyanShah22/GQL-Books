50f50b0e253cab17a4aa631f6c2cdf38dbd69401
DhyanShah22 <dhyanshah229@gmail.com>
2023-12-24 14:53:19 +0530

    Resolvers ready

---
+++ GraphQL/resolvers.js
@@ -0,0 +1,45 @@
+const Book = require('../Models/book')
+
+module.exports = {
+    Query: {
+        async book(_, {ID}) {
+            return await Book.findById(ID)
+        },
+        async getBook(_, {amount}) {
+            return await Book.find().sort({createdAt: -1}).limit(amount)
+        }
+    },
+    Mutation: {
+        async createBook(_, {bookInput: {BookName, Description, AuthorName, Review, Rating}}) {
+            const createdRecipe = new Recipe ({
+                BookName: BookName,
+                Description: Description,
+                AuthorName: AuthorName,
+                Review: Review,
+                Rating: Rating
+            })
+
+            const res = await createdBook.save()
+            console.log(res._doc)
+
+            return {
+                id: res.id,
+                ...res._doc
+            }
+        },
+        async deleteBook(_, {ID}) {
+            const wasDeleted = (await Book.deleteOne({_id :ID})).deletedCount
+            return wasDeleted
+        },
+        async editBook(_, {ID, bookInput: {BookName, Description, AuthorName, Review, Rating}}) {
+            const wasEdited = (await Book.updateOne({_id: ID}, {
+                BookName: BookName,
+                Description: Description,
+                AuthorName: AuthorName,
+                Review: Review,
+                Rating: Rating
+            })).modifiedCount
+            return wasEdited
+        }
+    }
+}
